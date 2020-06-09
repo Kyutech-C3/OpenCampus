32afcc9799c932019afbc12ef4628c84cead2fc6
Ryota_K <mas3920gml@icloud.com>
2020-06-09 16:24:22 +0900

    ハンバーガーメニューからのページスクロールを実装

---
+++ js/scroll.js
@@ -0,0 +1,27 @@
+function scrollWeb(){
+  document.getElementById("web").scrollIntoView(true);
+}
+
+function scrollGame3D(){
+  document.getElementById("game3D").scrollIntoView(true);
+}
+
+function scrollGame2D(){
+  document.getElementById("game2D").scrollIntoView(true);
+}
+
+function scrollLineBot(){
+  document.getElementById("lineBot").scrollIntoView(true);
+}
+
+function scrollModeling(){
+  document.getElementById("modeling").scrollIntoView(true);
+}
+
+function scrollClubPresident(){
+  document.getElementById("clubPresident").scrollIntoView(true);
+}
+
+function scrollClubJournal(){
+  document.getElementById("clubJournal").scrollIntoView(true);
+}
