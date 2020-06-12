2474f65276d2b782ae5c3e391cf032e6f40d0184
rkun <rkun123@hotmail.com>
2020-06-12 14:10:37 +0900

    モデルの定義とマイグレーションの生成

--- server/works/admin.py
+++ server/works/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Team, Work, Comment, Game, Video, Model3D
 
 # Register your models here.
+
+admin.site.register(Team)
+admin.site.register(Work)
+admin.site.register(Comment)
+admin.site.register(Game)
+admin.site.register(Video)
+admin.site.register(Model3D)
--- server/works/models.py
+++ server/works/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+
+class Work(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(default="None")
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    work = models.ForeignKey(Work, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    text = models.TextField()
+
+class Game(models.Model):
+    unityroom_url = models.CharField(max_length=255)
+
+class Video(models.Model):
+    url = models.CharField(max_length=255)
+
+class Model3D(models.Model):
+    sketchfab_id = models.CharField(max_length=255)
