2e945337e5b1ec0c770b775775db67f1eba1ef3c
Ryota_K <mas3920gml@icloud.com>
2020-06-20 17:24:39 +0900

    作品カードのフェードアウトを実装（仮）

--- front_end/js/fadeInOut.js
+++ front_end/js/fadeInOut.js
@@ -1,17 +1,32 @@
 $(function(){
 	$(window).scroll(function(){
-		/* fadeinクラスを持つ要素に対して処理  */
-		$('.fadein').each(function(){
-			var elemPos = $(this).offset().top;	// ドキュメント上での表示位置（場合topのみ取得）
-			var scroll = $(window).scrollTop();	// 要素の現在のスクロール上の上位置を取得
+		/* worksクラスを持つ要素に対して処理  */
+		$('.works').each(function(){
+			var elemPos = $(this).offset().top;	// ドキュメント上での表示位置（topのみ取得）
+			var scroll = $(window).scrollTop();	// 現在のスクロール上の上位置を取得
 			var windowHeight = $(window).height();	//ウインドウの高さをピクセル単位で取得
+
+			if(scroll <= elemPos){
+				$(this).addClass('up');
+				$(this).removeClass('down');
+			}
+			else{
+				$(this).addClass('down');
+				$(this).removeClass('up');
+			}
 			// 画面内に入って200pxスクロールしたら
-			if (scroll > elemPos - windowHeight + 200){
-				// scrollin クラスを追加		
+			if ((scroll > elemPos - windowHeight + 200)){
+				// scrollin クラスを追加
 				$(this).addClass('scrollin');
+				$(this).removeClass('scrollout');
+			}
+			else if((scroll < -elemPos + windowHeight + 200)){
+				$(this).addClass('scrollout');
+				$(this).removeClass('scrollin');
 			}
 			else{
 				$(this).removeClass('scrollin');
+				$(this).removeClass('scrollout');
 			}
 		});
 	});
