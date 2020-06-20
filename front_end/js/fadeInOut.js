f4ee7d21b4c859a162090f02281e1b3d219a33b3
Ryota_K <mas3920gml@icloud.com>
2020-06-21 03:11:01 +0900

    スマホで見た時の作品カードのフェードイン/アウトの変更

--- front_end/js/fadeInOut.js
+++ front_end/js/fadeInOut.js
@@ -5,6 +5,8 @@ $(function(){
 			var elemPos = $(this).offset().top;	// ドキュメント上での表示位置（topのみ取得）
 			var scroll = $(window).scrollTop();	// 現在のスクロール上の上位置を取得
 			var windowHeight = $(window).height();	//ウインドウの高さをピクセル単位で取得
+			var fadeinOffset;
+			var fadeoutOffset = 400;
 
 			if(scroll <= elemPos){
 				$(this).addClass('up');
@@ -14,13 +16,20 @@ $(function(){
 				$(this).addClass('down');
 				$(this).removeClass('up');
 			}
+			// スマホの場合
+			if(isSmartPhone()){
+				fadeinOffset = 100;
+			}
+			else{
+				fadeinOffset = 200;
+			}
 			// 画面内に入って200pxスクロールしたら
-			if ((scroll > elemPos - windowHeight + 200)){
+			if ((scroll > elemPos - windowHeight + fadeinOffset)){
 				// scrollin クラスを追加
 				$(this).addClass('scrollin');
 				$(this).removeClass('scrollout');
 			}
-			else if((scroll < -elemPos + windowHeight + 200)){
+			else if((scroll < -elemPos + windowHeight + fadeoutOffset)){
 				$(this).addClass('scrollout');
 				$(this).removeClass('scrollin');
 			}
