21f078a575ba362c2451bb33c407c7e56209cd20
Ryota_K <mas3920gml@icloud.com>
2020-06-29 00:37:37 +0900

    スマホの時ハンバーガーメニューからも試遊ページへ飛べないように修正

--- front_end/js/removecontent.js
+++ front_end/js/removecontent.js
@@ -1,9 +1,16 @@
 $(function(){
-		/* worksクラスを持つ要素に対して処理  */
+		/* gotoPlayWindowクラスを持つ要素に対して処理  */
 		$('.gotoPlayWindow').each(function(){
 			// スマホやタブレットの場合
 			if(isSmartPhone2()){
 				$('.gotoPlayWindow').remove();
 			}
 		});
+    /* global-nav__itemクラスを持つ要素に対して処理  */
+    $('.global-nav__item').each(function(){
+      // スマホやタブレットの場合
+      if(isSmartPhone2()){
+        $('.global-nav__item').remove();
+      }
+    });
 });
