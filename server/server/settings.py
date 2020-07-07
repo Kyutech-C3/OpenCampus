d73c25353bcfdc832fe1247a9a2792b3b7c98f0b
rkun <tren111213@gmail.com>
2020-07-08 00:48:22 +0900

    Updated some designs

--- server/server/settings.py
+++ server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ndlw&sdtzb5r7f3a!c!=90wddvmv58r3!ge_h)zau=yi+8y1@x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
