c0e003c30f157cbcf198de432beb0a14de7497f8
rkun <tren111213@gmail.com>
2020-07-11 12:04:26 +0900

    Add comment feature

--- server/works/models.py
+++ server/works/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 from colorfield.fields import ColorField
 from colour import Color
 
@@ -82,9 +83,13 @@ class Work(models.Model):
         return reverse("work", args=[str(self.id)])
 
 class Comment(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
     work = models.ForeignKey(Work, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     text = models.TextField()
     
     def __str__(self):
         return "[{}] {}".format(self.name, self.text)
+
+    def ja_created_at_str(self):
+        return self.created_at.strftime("%Y/%m/%d %H:%M")
--- server/works/urls.py
+++ server/works/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='works'),
     path('<int:work_id>/', views.detail, name='work'),
     path('<int:work_id>/goods', views.goods, name='work.goods'),
+    path('<int:work_id>/comment', views.comment, name='work.comment'),
 ]
