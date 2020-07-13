956fd72aa495f45535ea2c3e177eeae3001bf756
rkun <tren111213@gmail.com>
2020-07-14 00:08:25 +0900

    [Fix] Fixed Detail button for smartphones

--- server/works/static/works/js/removecontent.js
+++ server/works/static/works/js/removecontent.js
@@ -1,11 +1,4 @@
 $(function(){
-		/* gotoPlayWindowクラスを持つ要素に対して処理  */
-		$('.gotoPlayWindow').each(function(){
-			// スマホやタブレットの場合
-			if(isSmartPhone2()){
-				$('.gotoPlayWindow').remove();
-			}
-		});
     /* global-nav__itemクラスを持つ要素に対して処理  */
     $('.global-nav__item').each(function(){
       // スマホやタブレットの場合
