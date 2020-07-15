634d4936f82a06a30896c3f17f774548ac39ee8c
rkun <tren111213@gmail.com>
2020-07-16 01:15:32 +0900

    [Fix] Fixed Digitalocean settings

--- server/server/settings/production.py
+++ server/server/settings/production.py
@@ -64,8 +64,12 @@ AUTH_PASSWORD_VALIDATORS = [
 AWS_ACCESS_KEY_ID=os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY=os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_ENDPOINT_URL = 'https://sgp1.digitaloceanspaces.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'media'
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-S3_URL = 'https://%s.sgp1.digitaloceanspaces.com/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_URL = S3_URL
+MEDIA_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
